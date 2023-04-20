c1d9e1a4608e00ecb8b925eb410a72e05f6fd3f1
vtsvykh <v.tsvykh0@gmail.com>
2023-04-20 11:38:33 +0700

    add comment

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ Group:
 Fishchukova Sofia
 Tsvykh Viktoria
 """
+
+import datetime as DT
+import ru_local as ru
+
+# Resident or nor resident
+
+# Tax property
+
+# Tax worker
+
+# Tax devident
+
+# Tax rent
+
+# Tax prize
+
+# Sum amount
